7b6ef9ee31f86c1df86708ac57a286dc044cafc9
Ezekiel A. Mitchell <ezekielamitchell@outlook.com>
2022-06-13 04:53:42 -0700

    Create BigO.java

---
+++ src/main/java/io/ezekielmitchell/Chapter1/BigONotation/BigO.java
@@ -0,0 +1,30 @@
+package io.ezekielmitchell.Chapter1.BigONotation;
+
+public class BigO {
+
+    // Big O notation tells me how fast an algorithm is
+    // runtimes for binary and simple search do NOT grow at the same rate
+    // Big O allows me to compare the number of operations of an algorithm
+    //      to tell me how fast it grows
+
+    // Simple Search complexity: O(n) worst case
+
+        /* common Big O run times:
+
+        O(log n) == long time (Binary Search)
+        O(n) == linear time (Simple Search)
+        O(n * Log n) (Quicksort)
+        O(n^2) (Selection Sort)
+        O(n!) (Travelings Salesperson)
+
+         */
+
+    // algorithm speed isn't measured in time but in growth of the number of operations
+    // O(log n) is a lot faster than O(n) but it gets a lot faster as the list size increases
+
+    public static void main(String[] args) {
+
+    }
+
+    
+}
