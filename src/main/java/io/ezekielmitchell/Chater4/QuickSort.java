796638f8e7d16a107c1113759878ef1277b80f9e
Ezekiel A. Mitchell <ezekielamitchell@outlook.com>
2022-06-20 17:01:14 -0700

    QuickSort

--- src/main/java/io/ezekielmitchell/Chater4/QuickSort.java
+++ src/main/java/io/ezekielmitchell/Chater4/QuickSort.java
@@ -1,5 +1,8 @@
 package io.ezekielmitchell.Chater4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class QuickSort {
 
     static QuickSort qs =  new QuickSort();
@@ -8,9 +11,15 @@ public class QuickSort {
     int max = 0;
 
     public static void main(String[] args) {
-        int[] arr = {1, 601, 33, 5, 89};
+        int[] arr = new int[10];
+        Random random = new Random(10);
 
-        System.out.println(qs.max(arr));
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i] = random.nextInt(0, 100);
+        }
+
+        System.out.println("Unsorted " + Arrays.toString(arr));
+        System.out.println("Quick sorted " + Arrays.toString(quicksort(arr, 0, arr.length-1)));
 
     }
 
@@ -39,6 +48,48 @@ public class QuickSort {
 
     public int max(int[] arr) {
         return maxX(arr, 0);
+    }
+
+    public static int[] quicksort(int[] array, int lowIndex, int highIndex) {
+
+        if (lowIndex >= highIndex) {
+            return array;
+        }
+
+        int pivot = array[highIndex];
+
+        int leftPointer = getLeftPointer(array, lowIndex, highIndex, pivot);
+
+        quicksort(array, lowIndex, leftPointer - 1);
+        quicksort(array, leftPointer + 1, highIndex);
+
+        return array;
+    }
+
+    private static int getLeftPointer(int[] array, int lowIndex, int highIndex, int pivot) {
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        while (leftPointer < rightPointer) {
+            while  (array[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+
+            swap(array, leftPointer, rightPointer);
+        }
+
+        swap(array, leftPointer, highIndex);
+        return leftPointer;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 
 
