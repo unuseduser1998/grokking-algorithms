9d3fd7276b5c68c8a74fd7a24ba0295c0f687f9d
Ezekiel A. Mitchell <ezekielamitchell@outlook.com>
2022-06-27 03:05:00 -0700

    Create BreadthFirstSearch.java

---
+++ src/main/java/io/ezekielmitchell/Chapter6/BreadthFirstSearch.java
@@ -0,0 +1,32 @@
+package io.ezekielmitchell.Chapter6;
+
+
+public class BreadthFirstSearch {
+
+
+    //Breadth-first algorithm: allows me to find the shortest distance between two things
+    //commonly used in graphs and other 'shortest path problems'
+    // Answers two questions
+    // 1) Is there a path from node A to node B?       2) What is the shortest path from node A to node B?
+
+    // Graph: a set of connections
+    // Graphs are made up of nodes and edges -- nodes can be connected to many other nodes (neighbors)
+    // Graphs model how different things are connected to one another
+
+    // Queues are similar to stacks -- I cannot random search through a queue, only enqueue and dequeue
+    // Queues are FIFO, Stacks are LIFO data structures
+
+    // Complexity: O(V+E)
+
+    public static void main(String[] args) {
+
+
+    }
+
+    void Graph(int size) {
+
+
+    }
+
+
+}
